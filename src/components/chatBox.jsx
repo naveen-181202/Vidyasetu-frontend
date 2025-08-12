f7222430b27533f9409e5399b81086260f14ce68
Bhukya Naveen <167703671+naveen-181202@users.noreply.github.com>
2025-08-12 15:46:18 +0530

    Update chatBox.jsx

--- src/components/chatBox.jsx
+++ src/components/chatBox.jsx
@@ -12,7 +12,7 @@ const ChatBox = ({ user }) => {
   const token = localStorage.getItem("token");
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/chat", {
+      .get("https://vidyasetu-ceye.onrender.com", {
         headers: { token },
       })
       .then((res) => setMessages(res.data))
@@ -34,7 +34,7 @@ const ChatBox = ({ user }) => {
 
     try {
       await axios.post(
-        "http://localhost:5000/api/chat",
+        "https://vidyasetu-ceye.onrender.com",
         { text },
         {
           headers: { token },
